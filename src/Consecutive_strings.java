326f02f34dbdbf89b40be5adf179d6fd5e553f57
Bill Imus <bilimus@gmail.com>
2021-03-14 09:23:41 +0500

    commit

--- src/Consecutive_strings.java
+++ src/Consecutive_strings.java
@@ -1,14 +1,16 @@
 public class Consecutive_strings {
     public static void main(String[] args) {
-        String[] strarr = {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"};
-        int k = 4;
+        //String[] strarr = {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"};
+        //String[] strarr = {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"};
+        String[] strarr = {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"};
+        int k = 1;
 
         int lnStr = 0;
         int tmpLnStr = 0;
         String longStr = "";
         String finalStr = "";
 
-        for (int i = 0; i < strarr.length - k; i++) {
+        for (int i = 0; i <= strarr.length - k; i++) {
             for (int j = 0; j < k; j++) {
                 longStr += strarr[i + j];
             }
